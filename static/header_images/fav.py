bdf0302073284f6ede129448dc89cd1146cdb952
Nicholas Edger <nicholas.edger@gmail.com>
2025-08-07 18:13:51 +0000

    Updated with more fav icons

--- static/header_images/fav.py
+++ static/header_images/fav.py
@@ -1,10 +1,5 @@
 from PIL import Image
 
-# Load the original image
-img = Image.open("fav.webp")
-
-# Resize (optional, common sizes: 16x16, 32x32, 48x48)
-img = img.resize((32, 32))
-
-# Save as .ico
-img.save("favicon.ico", format="ICO")
+img = Image.open("fav.webp")  # or .png/.jpg
+img = img.resize((180, 180))
+img.save("apple-touch-icon.png", format="PNG")
